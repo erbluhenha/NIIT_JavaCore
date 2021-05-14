a009a1e258e338fd274ab772d38953d804d1b7a0
Long <longhh02@gmail.com>
2021-05-15 00:25:38 +0700

    update

--- Ex2/src/Ex1.java
+++ Ex2/src/Ex1.java
@@ -6,19 +6,32 @@ public class Ex1 {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		int diem;
+		int bonus=0;
 		String name,hang="";
 		System.out.print("Nhập tên: ");
 		name=sc.nextLine();
 		System.out.print("Nhập điểm: ");
 		diem=sc.nextInt();
-		if(diem>=0 && diem <10) {
+		if(diem>=10 && diem <15) {
+			bonus=1;
+		}else if(diem<=20) {
+			bonus=3;
+		}else if(diem<25) {
+			bonus=5;
+		}else {
+			bonus=0;
+		}
+		int temp=bonus+diem;
+		if(temp>=0 && temp<10) {
 			hang="D";
-		}else if(diem<15) {
+		}else if(temp<15) {
 			hang="C";
-		}else if(diem<20) {
+		}else if(temp<20) {
 			hang="B";
-		}else if(diem<30) {
+		}else if(temp<30) {
 			hang="A";
+		}else {
+			hang="Khong hang";
 		}
 		System.out.println("Họ tên: "+name);
 		System.out.println("Xếp loại: "+hang);
--- Ex2/src/Ex3.java
+++ Ex2/src/Ex3.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class Ex3 {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
 		int tong = 0, n = 0;
 		for (int i = 201; i <= 600; i += 3) {
 			tong += i;
@@ -12,8 +11,9 @@ public class Ex3 {
 		System.out.println("Tong: " + tong);
 		System.out.println("Trung binh: " + tb);
 
+		System.out.print("So le chia het cho 7: ");
 		for (int i = 50; i <= 100; i++) {
-			if (i % 7==0 && i%2!=0) {
+			if (i % 7 == 0 && i % 2 != 0) {
 				System.out.print(i + " ");
 			}
 		}
@@ -21,7 +21,7 @@ public class Ex3 {
 		System.out.println();
 		System.out.print("Số lẻ: ");
 		while (i <= 50) {
-			if (i % 2 == 0) {
+			if (i % 2 != 0) {
 				System.out.print(i + " ");
 			}
 			i++;
